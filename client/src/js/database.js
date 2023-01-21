88309896e941a59421043a9fc0d6e7d54c068f8d
Eric Simmons <erictomlinsonsimmons@gmail.com>
2023-01-21 17:20:48 -0600

    install button/add and read from db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,25 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+//adds content to database
+export const putDb = async (content) => {
+  const jate = await openDB('jate', 1)
+  const get = jate.transaction('jate', 'readwrite')
+  const database = get.objectStore('jate')
+  const request = database.put({id: 1, value: content})
+  const result = await request
+  console.log(result)
+}
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+
+//gets content from database
+export const getDb = async () => {
+  const jate = await openDB('jate', 1)
+  const get = jate.transaction('jate', 'readonly')
+  const database = get.objectStore('jate')
+  const request = database.getAll()
+  const result = await request
+  return result
+}
 
 initdb();
